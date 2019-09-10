10f2998d96f7be55caedd755c8e8d1ade8cba807
Shahid Kamal <17620737+shahidcodes@users.noreply.github.com>
2019-09-11 00:08:20 +0530

    added build folder for gh-pages

---
+++ precache-manifest.9ec3e2e723764f4829ced57b14059724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40122668dc691c0bf274c3f141e3ee21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc75f846bdd221175064",
+    "url": "/static/css/2.3fc73323.chunk.css"
+  },
+  {
+    "revision": "4499143344063b6d2503",
+    "url": "/static/css/main.0b60e967.chunk.css"
+  },
+  {
+    "revision": "cc75f846bdd221175064",
+    "url": "/static/js/2.a6cfb49b.chunk.js"
+  },
+  {
+    "revision": "4499143344063b6d2503",
+    "url": "/static/js/main.590182f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
